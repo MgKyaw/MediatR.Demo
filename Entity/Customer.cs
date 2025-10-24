6a6ba535d279bb8594a0697ceae841ec2dacb92b
MgKyaw <mgkyaw20112@gmail.com>
2025-10-24 21:13:39 +0700

    feat: Create Customer model

---
+++ Entity/Customer.cs
@@ -0,0 +1,16 @@
+namespace MediatR.Demo.Entity;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    public int? Age { get; set; }
+
+    public string Phone { get; set; }
+}
